a9a15dbb3079c1bd1505ee96f1371825325039c7
Tuan <lhminhtuan2000@gmail.com>
2022-04-16 21:14:06 +0700

    first commit

---
+++ PROJECT/MH_Admin_1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.OracleClient;
+
+namespace PROJECT
+{
+    public partial class MH_Admin_1 : Form
+    {
+        string oradb = "Data Source=XEPDB1;User Id=system;Password=sys;";
+        DataTable dt = new DataTable();
+        public MH_Admin_1()
+        {
+            InitializeComponent();
+            dgv1_loaddata();
+        }
+        public void dgv1_loaddata()
+        {
+            try
+            {
+                using (OracleConnection con = new OracleConnection(oradb))
+                {
+                    //OracleCommand cmd = new OracleCommand("exec sp_list_all_user", con);
+                    OracleCommand cmd = new OracleCommand("SELECT username,password,default_tablespace FROM dba_users WHERE account_status = 'OPEN'", con);
+                    OracleDataAdapter oda = new OracleDataAdapter(cmd);
+                    dt.Clear();
+                    oda.Fill(dt);
+                    dgv1.DataSource = dt;
+                    dgv1.AutoResizeColumnHeadersHeight();
+                }
+            }
+            catch (OracleException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        public void dgv2_loaddata(string username)
+        {
+            try
+            {
+                using (OracleConnection con = new OracleConnection(oradb))
+                {
+                    //OracleCommand cmd = new OracleCommand("exec sp_show_user_privilges('" + username + "')", con);
+                    OracleCommand cmd = new OracleCommand("SELECT privilege,ADMIN_OPTION FROM DBA_SYS_PRIVS where grantee = UPPER(" + username + "')", con);
+                    OracleDataAdapter oda = new OracleDataAdapter(cmd);
+                    //dt.Clear();
+                    oda.Fill(dt);
+                    dgv2.DataSource = dt;
+                    dgv2.AutoResizeColumnHeadersHeight();
+                }
+            }
+            catch (OracleException ex)
+            {
+                //MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void dgv1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                int rowIndex = dgv1.CurrentCell.RowIndex;
+                string username = dgv1.Rows[rowIndex].Cells[0].Value.ToString();
+                string password = dgv1.Rows[rowIndex].Cells[1].Value.ToString();
+                tb1.Text = username;
+                tb2.Text = password;
+                dgv2_loaddata(username);
+            }
+            catch (OracleException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void bt_them_Click(object sender, EventArgs e)
+        {
+            string username = tb1.ToString();
+            string password = tb2.ToString();
+            try
+            {
+                using (OracleConnection con = new OracleConnection(oradb))
+                {
+                    OracleCommand cmd = new OracleCommand();
+                    cmd.Connection = con;
+                    cmd.CommandText = System.String.Concat("exec sp_create_user('" + username+ "', "  +password+ ")");
+                    cmd.CommandType = CommandType.Text;
+                    cmd.ExecuteNonQuery();
+                    dgv1_loaddata();
+                }
+            }
+            catch (OracleException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void bt_xoa_Click(object sender, EventArgs e)
+        {
+            string username = tb1.ToString();
+            string password = tb2.ToString();
+            try
+            {
+                using (OracleConnection con = new OracleConnection(oradb))
+                {
+                    OracleCommand cmd = new OracleCommand();
+                    cmd.Connection = con;
+                    cmd.CommandText = System.String.Concat("exec sp_delete_user('" +username+ ")");
+                    cmd.CommandType = CommandType.Text;
+                    cmd.ExecuteNonQuery();
+                    dgv1_loaddata();
+                }
+            }
+            catch (OracleException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void bt_sua_Click(object sender, EventArgs e)
+        {
+            string username = tb1.ToString();
+            string password = tb2.ToString();
+            try
+            {
+                using (OracleConnection con = new OracleConnection(oradb))
+                {
+                    OracleCommand cmd = new OracleCommand();
+                    cmd.Connection = con;
+                    cmd.CommandText = System.String.Concat("exec sp_change_user_password('" + username + "', " + password + ")");
+                    cmd.CommandType = CommandType.Text;
+                    cmd.ExecuteNonQuery();
+                    dgv1_loaddata();
+                }
+            }
+            catch (OracleException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void bt_khoa_Click(object sender, EventArgs e)
+        {
+            string username = tb1.ToString();
+            string password = tb2.ToString();
+            try
+            {
+                using (OracleConnection con = new OracleConnection(oradb))
+                {
+                    OracleCommand cmd = new OracleCommand();
+                    cmd.Connection = con;
+                    cmd.CommandText = System.String.Concat("exec sp_lock_user('" + username+ ")");
+                    cmd.CommandType = CommandType.Text;
+                    cmd.ExecuteNonQuery();
+                    dgv1_loaddata();
+                }
+            }
+            catch (OracleException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+    }
+}
---
+++ PROJECT/MH_Admin_2.Designer.cs
@@ -0,0 +1,364 @@
+﻿
+namespace PROJECT
+{
+    partial class MH_Admin_2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.bt_thuquyenrole = new System.Windows.Forms.Button();
+            this.bt_capquyenrole = new System.Windows.Forms.Button();
+            this.cb_quyen = new System.Windows.Forms.ComboBox();
+            this.cb_cot = new System.Windows.Forms.ComboBox();
+            this.cb_bang = new System.Windows.Forms.ComboBox();
+            this.checkBox = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgv2 = new System.Windows.Forms.DataGridView();
+            this.label = new System.Windows.Forms.Label();
+            this.vaiTròToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ngườiDùngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tùyChọnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.bt_xoa = new System.Windows.Forms.Button();
+            this.bt_them = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb2 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.bt_capquyen = new System.Windows.Forms.Button();
+            this.bt_thuquyen = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv2)).BeginInit();
+            this.menu.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.bt_thuquyenrole);
+            this.panel2.Controls.Add(this.bt_capquyenrole);
+            this.panel2.Controls.Add(this.cb_quyen);
+            this.panel2.Controls.Add(this.cb_cot);
+            this.panel2.Controls.Add(this.cb_bang);
+            this.panel2.Controls.Add(this.checkBox);
+            this.panel2.Location = new System.Drawing.Point(0, 292);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(301, 152);
+            this.panel2.TabIndex = 22;
+            // 
+            // bt_thuquyenrole
+            // 
+            this.bt_thuquyenrole.Location = new System.Drawing.Point(179, 104);
+            this.bt_thuquyenrole.Name = "bt_thuquyenrole";
+            this.bt_thuquyenrole.Size = new System.Drawing.Size(85, 38);
+            this.bt_thuquyenrole.TabIndex = 7;
+            this.bt_thuquyenrole.Text = "Thu quyền";
+            this.bt_thuquyenrole.UseVisualStyleBackColor = true;
+            // 
+            // bt_capquyenrole
+            // 
+            this.bt_capquyenrole.Location = new System.Drawing.Point(50, 104);
+            this.bt_capquyenrole.Name = "bt_capquyenrole";
+            this.bt_capquyenrole.Size = new System.Drawing.Size(85, 38);
+            this.bt_capquyenrole.TabIndex = 7;
+            this.bt_capquyenrole.Text = "Cấp quyền";
+            this.bt_capquyenrole.UseVisualStyleBackColor = true;
+            // 
+            // cb_quyen
+            // 
+            this.cb_quyen.FormattingEnabled = true;
+            this.cb_quyen.Items.AddRange(new object[] {
+            "SELECT",
+            "UPDATE",
+            "INSERT",
+            "DELETE"});
+            this.cb_quyen.Location = new System.Drawing.Point(27, 28);
+            this.cb_quyen.Name = "cb_quyen";
+            this.cb_quyen.Size = new System.Drawing.Size(108, 21);
+            this.cb_quyen.TabIndex = 8;
+            this.cb_quyen.Text = "Tên quyền";
+            // 
+            // cb_cot
+            // 
+            this.cb_cot.FormattingEnabled = true;
+            this.cb_cot.Location = new System.Drawing.Point(27, 62);
+            this.cb_cot.Name = "cb_cot";
+            this.cb_cot.Size = new System.Drawing.Size(108, 21);
+            this.cb_cot.TabIndex = 12;
+            this.cb_cot.Text = "Tên cột";
+            // 
+            // cb_bang
+            // 
+            this.cb_bang.FormattingEnabled = true;
+            this.cb_bang.Items.AddRange(new object[] {
+            "HSBA",
+            "HSBA_DV",
+            "BỆNHNHÂN",
+            "CSYT",
+            "NHÂNVIÊN"});
+            this.cb_bang.Location = new System.Drawing.Point(158, 28);
+            this.cb_bang.Name = "cb_bang";
+            this.cb_bang.Size = new System.Drawing.Size(108, 21);
+            this.cb_bang.TabIndex = 9;
+            this.cb_bang.Text = "Tên bảng";
+            // 
+            // checkBox
+            // 
+            this.checkBox.AccessibleDescription = "";
+            this.checkBox.AutoSize = true;
+            this.checkBox.Location = new System.Drawing.Point(158, 64);
+            this.checkBox.Name = "checkBox";
+            this.checkBox.Size = new System.Drawing.Size(140, 17);
+            this.checkBox.TabIndex = 10;
+            this.checkBox.Text = "WITH GRANT OPTION";
+            this.checkBox.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgv1);
+            this.groupBox2.Location = new System.Drawing.Point(308, 34);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(396, 252);
+            this.groupBox2.TabIndex = 20;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Danh sách các vai trò";
+            // 
+            // dgv1
+            // 
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Location = new System.Drawing.Point(6, 19);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.Size = new System.Drawing.Size(390, 233);
+            this.dgv1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgv2);
+            this.groupBox1.Location = new System.Drawing.Point(307, 292);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(397, 152);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin về quyền của vai trò";
+            // 
+            // dgv2
+            // 
+            this.dgv2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv2.Location = new System.Drawing.Point(6, 14);
+            this.dgv2.Name = "dgv2";
+            this.dgv2.Size = new System.Drawing.Size(391, 138);
+            this.dgv2.TabIndex = 0;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(75, 34);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(174, 23);
+            this.label.TabIndex = 18;
+            this.label.Text = "ROLE MANAGER";
+            // 
+            // vaiTròToolStripMenuItem
+            // 
+            this.vaiTròToolStripMenuItem.Name = "vaiTròToolStripMenuItem";
+            this.vaiTròToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.vaiTròToolStripMenuItem.Text = "Vai trò";
+            // 
+            // ngườiDùngToolStripMenuItem
+            // 
+            this.ngườiDùngToolStripMenuItem.Name = "ngườiDùngToolStripMenuItem";
+            this.ngườiDùngToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ngườiDùngToolStripMenuItem.Text = "Người dùng";
+            // 
+            // tùyChọnToolStripMenuItem
+            // 
+            this.tùyChọnToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ngườiDùngToolStripMenuItem,
+            this.vaiTròToolStripMenuItem});
+            this.tùyChọnToolStripMenuItem.Name = "tùyChọnToolStripMenuItem";
+            this.tùyChọnToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.tùyChọnToolStripMenuItem.Text = "Tùy chọn";
+            // 
+            // menu
+            // 
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tùyChọnToolStripMenuItem});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(705, 24);
+            this.menu.TabIndex = 17;
+            this.menu.Text = "menu";
+            // 
+            // bt_xoa
+            // 
+            this.bt_xoa.Location = new System.Drawing.Point(179, 61);
+            this.bt_xoa.Name = "bt_xoa";
+            this.bt_xoa.Size = new System.Drawing.Size(85, 38);
+            this.bt_xoa.TabIndex = 6;
+            this.bt_xoa.Text = "Xóa";
+            this.bt_xoa.UseVisualStyleBackColor = true;
+            // 
+            // bt_them
+            // 
+            this.bt_them.Location = new System.Drawing.Point(50, 61);
+            this.bt_them.Name = "bt_them";
+            this.bt_them.Size = new System.Drawing.Size(85, 38);
+            this.bt_them.TabIndex = 6;
+            this.bt_them.Text = "Thêm";
+            this.bt_them.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tên người dùng: ";
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(114, 23);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(150, 20);
+            this.tb1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tên vai trò: ";
+            // 
+            // tb2
+            // 
+            this.tb2.Location = new System.Drawing.Point(114, 122);
+            this.tb2.Name = "tb2";
+            this.tb2.Size = new System.Drawing.Size(150, 20);
+            this.tb2.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.bt_thuquyen);
+            this.panel1.Controls.Add(this.bt_capquyen);
+            this.panel1.Controls.Add(this.tb2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.tb1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.bt_them);
+            this.panel1.Controls.Add(this.bt_xoa);
+            this.panel1.Location = new System.Drawing.Point(0, 60);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(302, 226);
+            this.panel1.TabIndex = 21;
+            // 
+            // bt_capquyen
+            // 
+            this.bt_capquyen.Location = new System.Drawing.Point(50, 163);
+            this.bt_capquyen.Name = "bt_capquyen";
+            this.bt_capquyen.Size = new System.Drawing.Size(85, 38);
+            this.bt_capquyen.TabIndex = 7;
+            this.bt_capquyen.Text = "Cấp quyền";
+            this.bt_capquyen.UseVisualStyleBackColor = true;
+            // 
+            // bt_thuquyen
+            // 
+            this.bt_thuquyen.Location = new System.Drawing.Point(179, 163);
+            this.bt_thuquyen.Name = "bt_thuquyen";
+            this.bt_thuquyen.Size = new System.Drawing.Size(85, 38);
+            this.bt_thuquyen.TabIndex = 7;
+            this.bt_thuquyen.Text = "Thu quyền";
+            this.bt_thuquyen.UseVisualStyleBackColor = true;
+            // 
+            // MH_Admin_2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 448);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menu);
+            this.Name = "MH_Admin_2";
+            this.Text = "MH_Admin_2";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv2)).EndInit();
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button bt_thuquyenrole;
+        private System.Windows.Forms.Button bt_capquyenrole;
+        private System.Windows.Forms.ComboBox cb_quyen;
+        private System.Windows.Forms.ComboBox cb_cot;
+        private System.Windows.Forms.ComboBox cb_bang;
+        private System.Windows.Forms.CheckBox checkBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgv2;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.ToolStripMenuItem vaiTròToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ngườiDùngToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tùyChọnToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.Button bt_xoa;
+        private System.Windows.Forms.Button bt_them;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button bt_thuquyen;
+        private System.Windows.Forms.Button bt_capquyen;
+    }
+}
