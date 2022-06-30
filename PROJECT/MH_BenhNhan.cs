cc34018d4a1fefef0978660dad1c9095749d9e07
toaingo2108 <nqtoai2181@gmail.com>
2022-06-30 21:42:02 +0700

    final

--- PROJECT/MH_BenhNhan.cs
+++ PROJECT/MH_BenhNhan.cs
@@ -26,6 +26,25 @@ namespace PROJECT
             InitializeComponent();
             username = user_name;
             password = pass_word;
+            dgv1_loaddata();
+        }
+        public void dgv1_loaddata()
+        {
+            List<string> varList = new List<string>();
+            DataTable dt = new DataTable();
+
+            dt = Program.loadDT("qlbv_dba.SP_XEMTHONGTINCANHAN", username, password, varList, varList);
+            tb1.Text = dt.Rows[0]["macsyt"].ToString();
+            tb2.Text = dt.Rows[0]["tenbn"].ToString();
+            tb3.Text = dt.Rows[0]["cmnd"].ToString();
+            tb4.Text = dt.Rows[0]["ngaysinh"].ToString();
+            tb5.Text = dt.Rows[0]["sonha"].ToString();
+            tb6.Text = dt.Rows[0]["tenduong"].ToString();
+            tb7.Text = dt.Rows[0]["quanhuyen"].ToString();
+            tb8.Text = dt.Rows[0]["tinhtp"].ToString();
+            tb9.Text = dt.Rows[0]["tiensubenh"].ToString();
+            tb10.Text = dt.Rows[0]["tiensubenhgd"].ToString();
+            tb11.Text = dt.Rows[0]["diungthuoc"].ToString();
         }
         private void ThoátTSMI_Click(object sender, EventArgs e)
         {
--- PROJECT/MH_NhanVien.cs
+++ PROJECT/MH_NhanVien.cs
@@ -48,8 +48,24 @@ namespace PROJECT
             {
                 thôngBáoTSMT.Visible = true;
             }
+            dgv1_loaddata();
         }
+        public void dgv1_loaddata()
+        {
+            List<string> varList = new List<string>();
+            DataTable dt = new DataTable();
 
+            dt = Program.loadDT("qlbv_dba.SP_XEMTHONGTINCANHAN", username, password, varList, varList);
+            tb1.Text = dt.Rows[0]["hoten"].ToString();
+            cb1.Text = dt.Rows[0]["phai"].ToString();
+            tb3.Text = dt.Rows[0]["ngaysinh"].ToString();
+            tb4.Text = dt.Rows[0]["cmnd"].ToString();
+            tb5.Text = dt.Rows[0]["quequan"].ToString();
+            tb6.Text = dt.Rows[0]["sodt"].ToString();
+            tb7.Text = dt.Rows[0]["csyt"].ToString();
+            tb8.Text = dt.Rows[0]["vaitro"].ToString();
+            tb9.Text = dt.Rows[0]["chuyenkhoa"].ToString();
+        }
         private void HSBATSMI_Click(object sender, EventArgs e)
         {
             if (roleName == "NGHIENCUU")
